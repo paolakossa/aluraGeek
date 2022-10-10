b08baa879a03b046f56cc27033c291911bc09194
Paola Kossa <paolakossa@gmail.com>
2022-10-10 17:10:51 -0300

    Form validation and redirection to login page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+	/*Mostrar barra de pesquisa no mobile */ 
+
 	const input = document.querySelector('.input__cabecalho');
     const clique = document.querySelector('.input__cabecalho--lupa');
     
@@ -10,7 +12,75 @@
 
     })
 
+    /*Ir para a página de login*/
+
+    const btnLogin = document.querySelector('.botao__login'); 
+
+    btnLogin?.addEventListener('click', () => {
+    	window.location.href = 'file:///C:/Users/paola.kossa/OneDrive/Documentos/aluraGeek/login.html'
+    })
+
+/*Validação do formulário de envio de mensagem*/
+
+const nome = document.querySelector('.rodape__form--input');
+const labelNome = document.querySelector('.rodape__form--labelNome');
+let validNome = false; 
 
 
+const msg = document.querySelector('.rodape__form--msg');
+const labelMsg = document.querySelector('.rodape__form--labelMsg');
+let validMsg = false; 
 
-   
+nome.addEventListener('keyup', () => {
+
+	if(nome.value.length <= 2 ) {
+		labelNome.setAttribute('style', 'color: #FF4A4A');
+		labelNome.innerHTML = 'Nome *Insira, no mínimo, 3 caracteres';
+		nome.setAttribute('style', 'border-color:#FF4A4A');
+		validNome = false;
+	} else {
+		labelNome.setAttribute('style', 'color: #59CE8F');
+		labelNome.innerHTML = 'Nome';
+		nome.setAttribute('style', 'border-color:#59CE8F');
+		validNome = true;
+	}
+});
+
+msg.addEventListener('keyup', () => {
+
+	if(msg.value.length <= 6 ) {
+		labelMsg.setAttribute('style', 'color: #FF4A4A');
+		labelMsg.innerHTML = 'Este campo não pode estar vazio';
+		msg.setAttribute('style', 'border-color:#FF4A4A');
+		validMsg = false;
+	} else {
+		labelMsg.setAttribute('style', 'color: #59CE8F');
+		labelMsg.innerHTML = 'Escreva sua mensagem';
+		msg.setAttribute('style', 'border-color:#59CE8F');
+		validMsg = true;
+	}
+});
+
+const btnEnviarMensagem = document.querySelector('.rodape__form--botao');
+const msgSucesso = document.querySelector('.rodape__mensagem');
+
+btnEnviarMensagem.addEventListener('click', () => {
+
+		if(nome.value.length <= 2 && msg.value.length <= 6) {
+			msgSucesso.setAttribute('style', 'display:none');
+			msgSucesso.innerHTML = '';
+		
+
+	} else {
+
+		msgSucesso.setAttribute('style', 'display:block');
+		msgSucesso.innerHTML = 'Sua mensagem foi enviada com sucesso!';
+		nome.value = '';
+		msg.value = '';
+
+		
+	}
+
+
+		
+});
