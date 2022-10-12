37f1fd93b7c5021244a27eaa8733b88a22bbbbea
Paola Kossa <paolakossa@gmail.com>
2022-10-12 17:51:24 -0300

    Edit JavaScript Construction

--- js/deletarProduto.js
+++ js/deletarProduto.js
@@ -1,6 +1,15 @@
-const btnDeleta = document.querySelector('[data-tipo="lixeira1"]');
+const btnDeleta1 = document.querySelector('[data-tipo="lixeira1"]');
 const produto1 = document.querySelector('.todos__produtos--div1');
+const btnLapis1 = document.querySelector('[data-tipo="lapis1"]');
+const preco1 = document.querySelector('[data-tipo="preco1"]');
+const novo = document.createElement("input"); 
+const texto = document.querySelector('input')
 
-btnDeleta.addEventListener('click', ()=>{
+btnDeleta1.addEventListener('click', ()=>{
     produto1.remove();
-})
+  
+});
+
+btnLapis1.addEventListener('click', () => {
+    preco1.innerText = 'R$ 40,00';
+} )
