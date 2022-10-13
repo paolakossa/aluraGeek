ea2fc7c0e3c38a21bd54a2c890e243b84e8bb2fe
Paola Kossa <paolakossa@gmail.com>
2022-10-13 17:36:59 -0300

    JavaScript pages in construction

--- js/deletarProduto.js
+++ js/deletarProduto.js
@@ -1,15 +1,144 @@
 const btnDeleta1 = document.querySelector('[data-tipo="lixeira1"]');
 const produto1 = document.querySelector('.todos__produtos--div1');
-const btnLapis1 = document.querySelector('[data-tipo="lapis1"]');
-const preco1 = document.querySelector('[data-tipo="preco1"]');
-const novo = document.createElement("input"); 
-const texto = document.querySelector('input')
+
+const btnDeleta2 = document.querySelector('[data-tipo="lixeira2"]');
+const produto2 = document.querySelector('.todos__produtos--div2');
+
+const btnDeleta3 = document.querySelector('[data-tipo="lixeira3"]');
+const produto3 = document.querySelector('.todos__produtos--div3');
+
+const btnDeleta4 = document.querySelector('[data-tipo="lixeira4"]');
+const produto4 = document.querySelector('.todos__produtos--div4');
+
+const btnDeleta5 = document.querySelector('[data-tipo="lixeira5"]');
+const produto5 = document.querySelector('.todos__produtos--div5');
+
+const btnDeleta6 = document.querySelector('[data-tipo="lixeira6"]');
+const produto6 = document.querySelector('.todos__produtos--div6');
+
+const btnDeleta7 = document.querySelector('[data-tipo="lixeira7"]');
+const produto7 = document.querySelector('.todos__produtos--div7');
+
+const btnDeleta8 = document.querySelector('[data-tipo="lixeira8"]');
+const produto8 = document.querySelector('.todos__produtos--div8');
+
+const btnDeleta9 = document.querySelector('[data-tipo="lixeira9"]');
+const produto9 = document.querySelector('.todos__produtos--div9');
+
+const btnDeleta10 = document.querySelector('[data-tipo="lixeira10"]');
+const produto10 = document.querySelector('.todos__produtos--div10');
+
+const btnDeleta11 = document.querySelector('[data-tipo="lixeira11"]');
+const produto11 = document.querySelector('.todos__produtos--div11');
+
+const btnDeleta12 = document.querySelector('[data-tipo="lixeira12"]');
+const produto12 = document.querySelector('.todos__produtos--div12');
+
+const btnDeleta13 = document.querySelector('[data-tipo="lixeira13"]');
+const produto13 = document.querySelector('.todos__produtos--div13');
+
+const btnDeleta14 = document.querySelector('[data-tipo="lixeira14"]');
+const produto14 = document.querySelector('.todos__produtos--div14');
+
+const btnDeleta15 = document.querySelector('[data-tipo="lixeira15"]');
+const produto15 = document.querySelector('.todos__produtos--div15');
+
+const btnDeleta16 = document.querySelector('[data-tipo="lixeira16"]');
+const produto16 = document.querySelector('.todos__produtos--div16');
+
+const btnDeleta17 = document.querySelector('[data-tipo="lixeira17"]');
+const produto17 = document.querySelector('.todos__produtos--div17');
+
+const btnDeleta18 = document.querySelector('[data-tipo="lixeira18"]');
+const produto18 = document.querySelector('.todos__produtos--div18');
+
 
 btnDeleta1.addEventListener('click', ()=>{
     produto1.remove();
   
 });
 
-btnLapis1.addEventListener('click', () => {
-    preco1.innerText = 'R$ 40,00';
-} )
+btnDeleta2.addEventListener('click', ()=>{
+    produto2.remove();
+  
+});
+
+btnDeleta3.addEventListener('click', ()=>{
+    produto3.remove();
+  
+});
+
+btnDeleta4.addEventListener('click', ()=>{
+    produto4.remove();
+  
+});
+
+btnDeleta5.addEventListener('click', ()=>{
+    produto5.remove();
+  
+});
+
+btnDeleta6.addEventListener('click', ()=>{
+    produto6.remove();
+  
+});
+
+btnDeleta7.addEventListener('click', ()=>{
+    produto7.remove();
+  
+});
+
+btnDeleta8.addEventListener('click', ()=>{
+    produto8.remove();
+  
+});
+
+btnDeleta9.addEventListener('click', ()=>{
+    produto9.remove();
+  
+});
+
+btnDeleta10.addEventListener('click', ()=>{
+    produto10.remove();
+  
+});
+
+btnDeleta11.addEventListener('click', ()=>{
+    produto11.remove();
+  
+});
+
+btnDeleta12.addEventListener('click', ()=>{
+    produto12.remove();
+  
+});
+
+btnDeleta13.addEventListener('click', ()=>{
+    produto13.remove();
+  
+});
+
+btnDeleta14.addEventListener('click', ()=>{
+    produto14.remove();
+  
+});
+
+btnDeleta15.addEventListener('click', ()=>{
+    produto15.remove();
+  
+});
+
+btnDeleta16.addEventListener('click', ()=>{
+    produto16.remove();
+  
+});
+
+btnDeleta17.addEventListener('click', ()=>{
+    produto17.remove();
+  
+});
+
+btnDeleta18.addEventListener('click', ()=>{
+    produto18.remove();
+  
+});
